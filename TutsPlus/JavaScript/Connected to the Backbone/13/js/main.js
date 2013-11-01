48a9ecaa7158c2e618b1ede5a5efb276a28cdfd5
Rafal Wesolowski <info@styleax.de>
2013-11-01 20:54:59 +0100

    Hands On Tasks Custom Events

---
+++ TutsPlus/JavaScript/Connected to the Backbone/13/js/main.js
@@ -0,0 +1,106 @@
+(function() {
+
+    window.App = {
+        Models: {},
+        Collections : {},
+        Views: {}
+
+    };
+
+    window.template = function( id ) {
+        return _.template( $('#'+id).html() );
+    };
+
+    App.Models.Task = Backbone.Model.extend({
+
+        initialize : function(){
+            this.on("invalid",function(model,error){
+                console.log(error);
+            });
+        },
+
+        validate: function(attrs) {
+            if( ! $.trim( attrs.title ) ) {
+                return 'A task requires a valid title.';
+            }
+        }
+
+    });
+
+    App.Collections.Tasks = Backbone.Collection.extend({
+        model: App.Models.Task
+    });
+
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+
+        render: function() {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+
+        addOne: function( task ) {
+
+            var taskView = new App.Views.Task({ model: task });
+
+            this.$el.append( taskView.render().el );
+        }
+    });
+
+    App.Views.Task = Backbone.View.extend({
+
+        tagName: 'li',
+
+        template: template('taskTemplate'),
+
+        initialize: function() {
+            console.log(this.model);
+            // this.model.on('change:title')
+            this.model.on('change', this.render, this);
+
+        },
+
+        events: {
+            'click .edit': 'editTask'
+        },
+
+        editTask: function() {
+            var newTaskTitle = prompt('What would you like to change the text to?', this.model.get('title'));
+            if( !newTaskTitle )
+            {
+                return;
+            }
+            this.model.set({'title' : newTaskTitle}, {validate : true});
+        },
+
+        render: function() {
+
+            var template = this.template( this.model.toJSON() );
+            this.$el.html( template );
+            return this;
+        }
+
+    });
+
+
+    var tasksCollection = new App.Collections.Tasks([
+        {
+            title: 'Go to the store',
+            priotity: 4
+        },
+        {
+            title: 'Go to the mall',
+            priotity: 3
+        },
+        {
+            title: 'Get to work',
+            priotity: 5
+        }
+    ]);
+
+    var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+    tasksView.render();
+    $('.tasks').html(tasksView.el);
+
+
+})();
