f0d9ae4ad24c96e54755f5dfd709d285abe9957b
Rafal Wesolowski <info@styleax.de>
2014-01-04 22:24:53 +0100

    The Proxy Pattern

--- TutsPlus/PHP/Agile Design Patterns/04/CartProxy.php
+++ TutsPlus/PHP/Agile Design Patterns/04/CartProxy.php
@@ -5,8 +5,20 @@ class CartProxy implements Cart {
 	private $shoppinCart;
 
 	public function getProducts() {
+        // 1.
+        // $shoppinCart = new ShoppingCart();
+        // return $shoppinCart->getProducts();
+
+        //2.
+        // if(is_null($this->shoppinCart))
+        // {
+        //     $this->shoppingCart = new ShoppingCart();
+        // }
+
 		if(is_null($this->shoppinCart))
+        {
 			$this->shoppingCart = $gateway->getCarts();
+        }
 		return $this->shoppingCart->getProducts();
 	}
 }
