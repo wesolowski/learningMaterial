cf456bbd5d1d7286b94e9d1c0d36f2ccd4c8e61a
Rafal Wesolowski <info@styleax.de>
2014-07-20 22:03:50 +0200

    [Part 2] - Contact Page: Validators, Forms and Emailing

---
+++ Symfony/tutorial.symblog.co.uk/src/Blogger/BlogBundle/Form/EnquiryType.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Blogger\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EnquiryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+        $builder->add('email', 'email');
+        $builder->add('subject');
+        $builder->add('body', 'textarea');
+    }
+
+    public function getName()
+    {
+        return 'contact';
+    }
+}
