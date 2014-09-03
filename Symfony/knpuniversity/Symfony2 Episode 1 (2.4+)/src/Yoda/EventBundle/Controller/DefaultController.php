b051dacfa972729250fc1c03d481d1a0a2c74b36
Rafal Wesolowski <info@styleax.de>
2014-09-03 21:48:41 +0200

    Bundles of Joy!
    Routing: The URLs of the World
    
    Console Command:
    
    php app/console generate:bundle - Create Bundle
    
    php app/console router:debug - Show all Project Router

---
+++ Symfony/knpuniversity/Symfony2 Episode 1 (2.4+)/src/Yoda/EventBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Yoda\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($firstName, $count)
+    {
+        return $this->render('EventBundle:Default:index.html.twig', array('name' => $firstName));
+    }
+}
