de78856421a70b98d581b07b2e72c6fbd33e0038
Rafal Wesolowski <info@styleax.de>
2014-09-03 21:59:34 +0200

    Controllers: Get to Work

--- Symfony/knpuniversity/Symfony2 Episode 1 (2.4+)/src/Yoda/EventBundle/Controller/DefaultController.php
+++ Symfony/knpuniversity/Symfony2 Episode 1 (2.4+)/src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($firstName, $count)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $firstName));
+        $data = array(
+            'count' => $count,
+            'firstName' => $firstName,
+            'ackbar' => 'It\'s a traaaaap!'
+        );
+
+        $json = json_encode($data);
+
+        $response = new Response($json);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+        //2. return new Response('It\'s a traaaaap!');
+        //1. return $this->render('EventBundle:Default:index.html.twig', array('name' => $firstName));
     }
 }
