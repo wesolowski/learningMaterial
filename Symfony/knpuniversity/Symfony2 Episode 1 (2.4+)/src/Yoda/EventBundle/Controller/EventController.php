240d13eddb7b5db18461878cef8f662dfb106154
Rafal Wesolowski <info@styleax.de>
2014-09-06 08:15:45 +0200

    Do Less Work in the Controller

--- Symfony/knpuniversity/Symfony2 Episode 1 (2.4+)/src/Yoda/EventBundle/Controller/EventController.php
+++ Symfony/knpuniversity/Symfony2 Episode 1 (2.4+)/src/Yoda/EventBundle/Controller/EventController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Yoda\EventBundle\Entity\Event;
 use Yoda\EventBundle\Form\EventType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
  * Event controller.
@@ -16,6 +18,8 @@ class EventController extends Controller
 {
 
     /**
+     * @Route("/", name="event")
+     * @Template()
      * Lists all Event entities.
      *
      */
@@ -25,9 +29,9 @@ class EventController extends Controller
 
         $entities = $em->getRepository('EventBundle:Event')->findAll();
 
-        return $this->render('EventBundle:Event:index.html.twig', array(
+        return array(
             'entities' => $entities,
-        ));
+        );
     }
     /**
      * Creates a new Event entity.
