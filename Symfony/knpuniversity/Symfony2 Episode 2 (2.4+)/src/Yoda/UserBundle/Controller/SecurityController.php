90397494adb9046d9881c107ce11b0be4f0c0735
Rafal Wesolowski <info@styleax.de>
2014-09-08 07:19:07 +0200

    Login Form
    http://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/view.html
    
    http://symfony.com/doc/current/bundles/SensioFrameworkExtraBundle/annotations/routing.html
    
    http://symfony.com/doc/current/book/security.html#using-a-traditional-login-form

---
+++ Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Yoda\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+class SecurityController extends Controller
+{
+    /**
+     * @Template()
+     * @Route("/login", name="login_form")
+     */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return  array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        );
+
+    }
+}
