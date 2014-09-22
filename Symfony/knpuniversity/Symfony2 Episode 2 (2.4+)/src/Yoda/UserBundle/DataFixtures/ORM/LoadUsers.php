a4744bd99150d79a828789d3414691363a11366b
Rafal Wesolowski <info@styleax.de>
2014-09-22 06:59:26 +0200

    php app/console doctrine:fixtures:load
    php app/console doctrine:query:sql "SELECT * FROM yoda_user"
    
    php app/console assets:install --symlink
    
    php app/console assetic:dump --env=prod

---
+++ Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Yoda\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Yoda\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadUsers implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('darth');
+        $user->setPassword($this->encodePassword($user, 'darthpass'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    private function encodePassword(User $user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')
+                    ->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+}
