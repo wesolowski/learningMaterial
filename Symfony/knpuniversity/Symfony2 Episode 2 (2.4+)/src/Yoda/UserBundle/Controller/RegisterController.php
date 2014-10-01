11e4a3d84b3127529ba853735aca0837253af31f
Rafal Wesolowski <info@styleax.de>
2014-10-01 07:11:21 +0200

    Form Submit

--- Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Controller/RegisterController.php
+++ Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Controller/RegisterController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-
+use Yoda\UserBundle\Entity\User;
 
 class RegisterController extends Controller
 {
@@ -14,7 +14,7 @@ class RegisterController extends Controller
      * @Route("/register", name="user_register")
      * @Template()
      */
-    public function registerAction()
+    public function registerAction( Request $request )
     {
         $form = $this->createFormBuilder()
             ->add('username', 'text')
@@ -24,7 +24,34 @@ class RegisterController extends Controller
             ))
             ->getForm();
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $data = $form->getData();
+
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setEmail($data['email']);
+            $user->setPassword($this->encodePassword($user, $data['password']));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $url = $this->generateUrl('event');
+
+            return $this->redirect($url);
+        }
+
         return array('form' => $form->createView());
+    }
+
+    private function encodePassword(User $user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')
+                    ->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
     }
 }
 
