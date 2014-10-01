5b0b72777b0caaf5bf9bf60d23ea1a8d20232525
Rafal Wesolowski <info@styleax.de>
2014-10-01 06:57:47 +0200

    Form Rendering

--- Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Controller/RegisterController.php
+++ Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Controller/RegisterController.php
@@ -22,8 +22,7 @@ class RegisterController extends Controller
             ->add('password', 'password')
             ->getForm();
 
-        return array('form' => $form);
-
+        return array('form' => $form->createView());
     }
 }
 
