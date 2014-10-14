89983381cd70438707209901ea773beec720630a
Rafal Wesolowski <info@styleax.de>
2014-10-14 07:19:10 +0200

    Testing
    phpunit -c app

---
+++ Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Yoda\EventBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegisterControllerTest extends WebTestCase
+{
+    public function testRegister()
+    {
+        $client = static::createClient();
+
+        $container = self::$kernel->getContainer();
+
+        $crawler = $client->request('GET', '/register');
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Register', $response->getContent());
+
+        $usernameVal = $crawler
+            ->filter('#user_register_username')
+            ->attr('value');
+
+        $this->assertEquals('Leia', $usernameVal);
+    }
+}
