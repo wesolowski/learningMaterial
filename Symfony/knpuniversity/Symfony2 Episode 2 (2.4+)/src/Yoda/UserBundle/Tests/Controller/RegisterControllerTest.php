709bd4634ec91fd8dc590dadb354e429280287bb
Rafal Wesolowski <info@styleax.de>
2014-10-15 06:53:47 +0200

    Testing Forms

--- Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
+++ Symfony/knpuniversity/Symfony2 Episode 2 (2.4+)/src/Yoda/UserBundle/Tests/Controller/RegisterControllerTest.php
@@ -23,5 +23,31 @@ class RegisterControllerTest extends WebTestCase
             ->attr('value');
 
         $this->assertEquals('Leia', $usernameVal);
+
+        // the name of our button is "Register!"
+        $form = $crawler->selectButton('Register!')->form();
+        $crawler = $client->submit($form);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertRegexp(
+            '/This value should not be blank/',
+            $client->getResponse()->getContent()
+        );
+
+        // submit the form again
+        $form = $crawler->selectButton('Register!')->form();
+
+        $form['user_register[username]'] = 'user5';
+        $form['user_register[email]'] = 'user5@user.com';
+        $form['user_register[plainPassword][first]'] = 'P3ssword';
+        $form['user_register[plainPassword][second]'] = 'P3ssword';
+
+        $client->submit($form);
+
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $client->followRedirect();
+        $this->assertContains(
+            'Welcome to the Death Star! Have a magical day',
+            $client->getResponse()->getContent()
+        );
     }
 }
