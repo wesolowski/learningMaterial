0ae1cf699b1a2168abbdd4adb400e77348f967c1
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2018-07-09 22:51:49 +0200

    Babel Setup
    npm run build

---
+++ Laracast/javascript/ES2015 Crash Course/output/Person.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        return this.name + ' say hello.'; //review template string
+    }
+}
+
+console.log(new Person('Rafal').greet());
