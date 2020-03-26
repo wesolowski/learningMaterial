6631005854f7e384802b3bf396a90eb94d53ed6b
Rafal Wesolowski <r.wesolowski@nexus-united.com>
2020-03-26 23:34:20 +0100

    Working with calendars

--- Lynda/learning-python/calendars.py
+++ Lynda/learning-python/calendars.py
@@ -13,3 +13,27 @@ print(st)
 hc = calendar.HTMLCalendar(calendar.SUNDAY)
 st = hc.formatmonth(2017,1)
 print(st)
+
+for i in c.itermonthdays(2017,8):
+    print(i)
+
+
+for name in calendar.month_name:
+    print(name)
+
+
+for name in calendar.day_name:
+    print(name)
+
+print("Team meetings will be on:")
+for m in range(1,13):
+    cal = calendar.monthcalendar(2020, m)
+    weekone=cal[0]
+    weektwo=cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+    
+    print("%10s %2s" % (calendar.month_name[m], meetday))
